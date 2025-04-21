f1e295b59e218f8b52e9ea7102b2bfd77f195360
Panacond <ivachkevich63@gmail.com>
2025-04-21 21:42:16 +0300

    init

---
+++ svg-fragment.js
@@ -0,0 +1,7 @@
+const mySvgFragment = `
+  <g id="myReusableGroup">
+    <rect x="10" y="10" width="50" height="50" fill="orange" />
+    <circle cx="80" cy="35" r="20" fill="purple" />
+    <text id="replaceableText" x="10" y="70" font-size="12">Текст з JS</text>
+  </g>
+`;
